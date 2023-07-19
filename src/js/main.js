089471318dc27bbcf165060afecca227e178ee3d
Rajat Gupta <rajatgupta1798@gmail.com>
2023-07-19 23:16:39 +0530

    fix: mobile responsiveness

--- src/js/main.js
+++ src/js/main.js
@@ -30,9 +30,11 @@ submitButton.addEventListener("click", (e) => {
     alert("Please enter a valid number of floors and lifts");
   } else if (
     (store.lifts > 3 && window.innerWidth <= 400) ||
-    (store.floors > 6 && window.innerHeight < 650)
+    (store.floors > 6 && window.innerHeight < 700)
   ) {
-    alert("Please add less than 4 lifts & 7 floors to avoid scrolling");
+    alert(
+      "Please add less than 4 lifts & 7 floors for better viewing experience"
+    );
   } else {
     makeLobby();
   }
@@ -168,7 +170,7 @@ const makeLobby = ({ liftToOpen = null, targetFloor = null } = {}) => {
     });
   }
 
-  if (store.floors > 4) {
+  if (store.floors > 7) {
     window.scrollTo(0, document.body.scrollHeight);
   }
 };
